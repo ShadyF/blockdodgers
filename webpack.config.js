4e9f176a3aa8eeeeaab1f32f357ac9bff2c6fe1f
shadyf <shadster09@gmail.com>
2016-12-02 20:03:31 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+//require our dependencies
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    //the base directory (absolute path) for resolving the entry option
+    context: __dirname,
+    //the entry point we created earlier. Note that './' means
+    //your current directory. You don't have to specify the extension  now,
+    //because you will specify extensions later in the `resolve` section
+    entry: ['./assets/app'],
+
+    output: {
+        path: path.resolve('./build/'),
+        filename: "bundle.js"
+    },
+
+    plugins: [],
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            },
+        ]
+    },
+
+    resolve: {
+        //tells webpack where to look for modules
+        modulesDirectories: ['node_modules'],
+        //extensions that should be used to resolve modules
+        extensions: ['', '.js', '.jsx']
+    },
+
+    devtool: 'source-map'
+};
